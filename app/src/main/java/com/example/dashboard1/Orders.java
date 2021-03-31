6899c98dff6f565657e44194284cf87200769737
UzairAhmed7623 <hafiz.uzair579@gmail.com>
2021-03-31 17:49:21 +0500

    Commit 5!

--- app/src/main/java/com/example/dashboard1/Orders.java
+++ app/src/main/java/com/example/dashboard1/Orders.java
@@ -109,7 +109,6 @@ public class Orders extends AppCompatActivity implements OnMapReadyCallback {
         public void onDataChange(@NonNull DataSnapshot snapshot) {
             if (snapshot.exists() && currentUserRef != null) {
                 currentUserRef.onDisconnect().removeValue();
-                isFirstTime = true;
             }
         }
 
@@ -183,13 +182,11 @@ public class Orders extends AppCompatActivity implements OnMapReadyCallback {
     private void init() {
         onlineRef = FirebaseDatabase.getInstance().getReference().child("info/connected");
 
-        registerOnlineSystem();
-
         locationRequest = LocationRequest.create();
-//        locationRequest.setSmallestDisplacement(5f);
+        locationRequest.setSmallestDisplacement(20f);
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(5000);
-        locationRequest.setFastestInterval(1000);
+        locationRequest.setInterval(15000);
+        locationRequest.setFastestInterval(10000);
 
         locationCallback = new LocationCallback() {
             @Override
@@ -202,37 +199,6 @@ public class Orders extends AppCompatActivity implements OnMapReadyCallback {
                 Location location = locationResult.getLastLocation();
                 String id = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-                geoFire.setLocation(id, new GeoLocation(location.getLatitude() , location.getLongitude()));
-//                        new GeoFire.CompletionListener() {
-//                    @Override
-//                    public void onComplete(String key, DatabaseError error) {
-//                        if (error != null){
-//                            Snackbar.make(mapFragment.getView(), error.getMessage(), Snackbar.LENGTH_LONG).show();
-//                        }
-//                        else {
-//                            if (isFirstTime){
-//                                Snackbar.make(mapFragment.getView(), "You're online!", Snackbar.LENGTH_LONG).show();
-//                                isFirstTime = false;
-//                            }
-//                        }
-//                    }
-//                });
-
-            }
-        };
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
-
-        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-
-                LatLng newPosition = new LatLng(location.getLatitude(), location.getLongitude());
-                mgoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPosition, 18f));
-
                 Geocoder geocoder = new Geocoder(Orders.this, Locale.getDefault());
                 List<Address> addressList;
                 try {
@@ -244,17 +210,31 @@ public class Orders extends AppCompatActivity implements OnMapReadyCallback {
                     currentUserRef = driversLocationRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid());
                     geoFire = new GeoFire(driversLocationRef);
 
+                    geoFire.setLocation(id, new GeoLocation(location.getLatitude() , location.getLongitude()),
+                        new GeoFire.CompletionListener() {
+                    @Override
+                    public void onComplete(String key, DatabaseError error) {
+                        if (error != null){
+                            Snackbar.make(mapFragment.getView(), error.getMessage(), Snackbar.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+
+                    registerOnlineSystem();
 
                 } catch (IOException e) {
                     Snackbar.make(mapFragment.getView(), e.getMessage(), Snackbar.LENGTH_SHORT).show();
                 }
+
             }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Snackbar.make(mapFragment.getView(), e.getMessage(), Snackbar.LENGTH_LONG).show();
-            }
-        });
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+
     }
 
     @Override
@@ -311,99 +291,7 @@ public class Orders extends AppCompatActivity implements OnMapReadyCallback {
                 }
             }).check();
 
+            Snackbar.make(mapFragment.getView(), "You're online!", Snackbar.LENGTH_LONG).show();
     }
-
-//    private void getDirection() {
-//
-////        currentPosition = new LatLng(Common.latLng1.latitude, Common.latLng1.longitude);
-//        Toast.makeText(this, ""+currentPosition, Toast.LENGTH_SHORT).show();
-//
-//        String requestApi = null;
-//        try {
-//            requestApi = "https://maps.googleapis.com/maps/api/directions/json?" +
-//                    "mode=driving&" +
-//                    "transit_routing_preference=less_driving&" +
-//                    "origin=" + currentPosition.latitude + "," + currentPosition.longitude + "&" +
-//                    "destination=" + destination + "&" +
-//                    "key=" + "AIzaSyDl7YXtTZQNBkthV3PjFS0fQOKvL8SIR7k";
-//
-//            Log.d("TAG", requestApi);
-//
-//            mService.getPath(requestApi).enqueue(new Callback<String>() {
-//                @Override
-//                public void onResponse(Call<String> call, Response<String> response) {
-//                    try {
-//                        JSONObject jsonObject = new JSONObject(response.body().toString());
-//                        JSONArray jsonArray = jsonObject.getJSONArray("routes");
-//                        for (int i=0; i<jsonArray.length(); i++){
-//                            JSONObject route = jsonArray.getJSONObject(i);
-//                            JSONObject poly = route.getJSONObject("overview_polyline");
-//                            String polyline = poly.getString("points");
-//                            polylineList = decodePoly(polyline);
-//
-//                            Log.d("TAG", "" + polylineList);
-//
-//                            LatLngBounds.Builder builder = new LatLngBounds.Builder();
-//                            for (LatLng latLng : polylineList) {
-//                                builder.include(latLng);
-//                                LatLngBounds bounds = builder.build();
-//                                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 2);
-//                                mgoogleMap.animateCamera(cameraUpdate);
-//
-//
-//                                mgoogleMap.addMarker(new MarkerOptions().position(polylineList.get(polylineList.size() - 1)).title("Pickup Location"));
-//
-//                            }
-//                        }
-//                    } catch (JSONException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//
-//                @Override
-//                public void onFailure(Call<String> call, Throwable t) {
-//                    Toast.makeText(Orders.this, t.getMessage(), Toast.LENGTH_SHORT).show();
-//                }
-//            });
-//
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-//    private List decodePoly(String encoded) {
-//
-//        List poly = new ArrayList();
-//        int index = 0, len = encoded.length();
-//        int lat = 0, lng = 0;
-//
-//        while (index < len) {
-//            int b, shift = 0, result = 0;
-//            do {
-//                b = encoded.charAt(index++) - 63;
-//                result |= (b & 0x1f) << shift;
-//                shift += 5;
-//            } while (b >= 0x20);
-//            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-//            lat += dlat;
-//
-//            shift = 0;
-//            result = 0;
-//            do {
-//                b = encoded.charAt(index++) - 63;
-//                result |= (b & 0x1f) << shift;
-//                shift += 5;
-//            } while (b >= 0x20);
-//            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-//            lng += dlng;
-//
-//            LatLng p = new LatLng((((double) lat / 1E5)),
-//                    (((double) lng / 1E5)));
-//            poly.add(p);
-//        }
-//
-//        return poly;
-//    }
 
 }
